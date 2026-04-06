4b09e7d77c831f99cf6f062b4deda4d804b29e3b
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-06 17:37:28 +0700

    commit3

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -8,5 +8,9 @@ int main() {
     //deklarasi array tanpa inisialisasi
     int kamu[5];
     //tampilkan data(mwmbaca data array)
-    cout <<"Data pada index 1 = "<< aku[1 << endl;]
+    cout <<"Data pada index 1 = "<< aku[1]<< endl;
+    // ganti index ke 1(menulis data array)
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
 }
