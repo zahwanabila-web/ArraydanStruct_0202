775424bbde6d661860c7e66ddfa01a502845e0c1
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-06 18:30:24 +0700

    commit13

---
+++ structNew2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
