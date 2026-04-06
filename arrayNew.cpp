66ae85ed330421f688eb829ad0401a919d64cfc5
ZahwaNabila <zahwanabila434@gmail.com>
2026-04-06 17:34:07 +0700

    commit2

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main() {
     //deklarasi dengan inisialisasi
     int aku[5] = {10,20,30,40,50};
-    
+    //deklarasi array tanpa inisialisasi
+    int kamu[5];
+    //tampilkan data(mwmbaca data array)
+    cout <<"Data pada index 1 = "<< aku[1 << endl;]
 }
